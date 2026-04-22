20922845e06154d82961649cf209362e14eb25ad
KicaiBadai <adityamaula80@gmail.com>
2026-04-22 14:57:04 +0700

    auth data domain repositories

---
+++ lib/features/auth/data/domain/repositories/auth_repository_impl.dart
@@ -0,0 +1,18 @@
+
+import 'package:backend_firebase/core/services/dio_client.dart';
+import 'package:backend_firebase/core/constants/api_constants.dart';
+import 'package:backend_firebase/features/auth/data/domain/repositories/auth_repository.dart';
+
+
+class AuthRepositoryImpl implements AuthRepository {
+  @override
+  Future<String> verifyFirebaseToken(String firebaseToken) async {
+    final response = await DioClient.instance.post(
+      ApiConstants.verifyToken,
+      data: {'firebase_token': firebaseToken},
+    );
+
+    final data = response.data['data'] as Map<String, dynamic>;
+    return data['access_token'] as String;
+  }
+}
